ab452c7e38dbad7620acdeb6f22562f6f2d00629
jawwadturabi <memonjawwad9@gmail.com>
2020-04-14 04:15:56 +0500

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+use bson::doc;
 use mongodb::{options::ClientOptions, Client};
+use serde_json::{json, Value};
 //
 fn main() {
-    // Parse a connection string into an options struct.
-    let mut client_options = ClientOptions::parse("mongodb+srv://author:author123@cluster0-geoiq.mongodb.net/test?retryWrites=true&w=majority").unwrap();
-
-    // Manually set an option.
-    client_options.app_name = Some("My App".to_string());
-
-    // Get a handle to the deployment.
-    let client = Client::with_options(client_options).unwrap();
-
-    // List the names of the databases in that deployment.
-    for db_name in client.list_database_names(None).unwrap() {
-        println!("{}", db_name);
+    let client= Client::with_uri_str("mongodb+srv://author:author123@cluster0-geoiq.mongodb.net/test?retryWrites=true&w=majority").expect("Failed to connect");
+    let db = client.database("test").collection("user_details");
+    let docs = doc! { "user_name": "John123" };
+    let data = db.insert_one(docs, None).unwrap();
+    println!("id is : {}", data.inserted_id);
+    let docs1 = doc! { "user_name": "John","password":"1244","email":"abc@asd.com" };
+    let data = db.find_one(docs1,None).unwrap();
+    match data {
+        Some(data) => {
+            let data: Value = json!(data);
+            println!("data is : {}", data["user_name"]);
+        }
+        None => println!("No record Found"),
     }
 }
